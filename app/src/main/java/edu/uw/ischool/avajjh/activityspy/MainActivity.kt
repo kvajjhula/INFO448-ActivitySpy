167d98c3f46f2a9322f412a1ae81798016233050
kvajjhula <kriti.vajjhula@gmail.com>
2023-10-19 17:01:14 -0700

    add missing log for onDestroy and savedInstanceState

--- app/src/main/java/edu/uw/ischool/avajjh/activityspy/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/avajjh/activityspy/MainActivity.kt
@@ -10,32 +10,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i(TAG, "onCreate event fired");
+        Log.i(TAG, "onCreate event fired")
+        Log.i(TAG, savedInstanceState.toString())
     }
 
     override fun onStart() {
         super.onStart()
-        Log.i(TAG, "onStart event fired");
+        Log.i(TAG, "onStart event fired")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.i(TAG, "onResume event fired");
+        Log.i(TAG, "onResume event fired")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.i(TAG, "onStop event fired");
+        Log.i(TAG, "onStop event fired")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.i(TAG, "onPause event fired");
+        Log.i(TAG, "onPause event fired")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.e(TAG, "We're going down captain!");
+        Log.i(TAG, "onDestroy event fired")
+        Log.e(TAG, "We're going down captain!")
 
     }
 }
